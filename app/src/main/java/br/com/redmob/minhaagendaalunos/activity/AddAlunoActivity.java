f9cbf9a8ca7472dc177dcc6b678de4b2bf582b3f
bytecore@gmail.com <mo10128>
2018-08-24 14:26:27 -0300

    Refatora o medodo de sincronização

--- app/src/main/java/br/com/redmob/minhaagendaalunos/activity/AddAlunoActivity.java
+++ app/src/main/java/br/com/redmob/minhaagendaalunos/activity/AddAlunoActivity.java
@@ -24,7 +24,7 @@ public class AddAlunoActivity extends AppCompatActivity {
     private EditText editTelefone;
     private EditText editSite;
     private EditText editNota;
-    private Aluno alunoClicado;
+    private Alun alunoClicado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/br/com/redmob/minhaagendaalunos/firebase/AgendaInstanceIdService.java
+++ app/src/main/java/br/com/redmob/minhaagendaalunos/firebase/AgendaInstanceIdService.java
@@ -4,7 +4,6 @@ import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
-
 import br.com.redmob.minhaagendaalunos.retrofit.RetrofitInicializador;
 import retrofit2.Call;
 import retrofit2.Callback;
